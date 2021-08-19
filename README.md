029f43d4080dcb182405075c48b048048688ecb1
Clarissa P <clarissapamonicutt@gmail.com>
2021-08-18 23:05:13 -0700

    on first card

--- README.md
+++ README.md
@@ -17,9 +17,6 @@ Clone this repo to your own account with the green "Use this template" button. T
 **Author**: Team Member Names Goes Here
 **Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
 
-## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
-
 ## Getting Started
 <!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
 
@@ -104,3 +101,17 @@ Start time: _____
 Finish time: _____
 
 Actual time needed to complete: _____
+
+---
+
+Name of feature: Lab 12
+
+Estimate of time needed to complete: _____
+
+Start time: 8/18
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+---
--- server.js
+++ server.js
@@ -6,11 +6,24 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 const { request, response } = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+
+const BookModel = require('./modules/books');
+
+app.get('/books', async (request, response) => {
+  try {
+    let booksdb = await BookModel.find({});
+    response.status(200).send(booksdb);
+  }
+  catch (error) {
+    response.status(500).send('database error');
+  }
+});
 
 app.get('/', (request, response) => {
   response.send('Hello World!')
@@ -24,9 +37,9 @@ app.get('/test', (request, response) => {
   // ? What are the headers? The const/requires?
 
   const token = req.headers.authorizatoion.split(' ')[1];
-  
-  jwt.verify(token, getKey, {}, function (error, user){
-    if(error){
+
+  jwt.verify(token, getKey, {}, function (error, user) {
+    if (error) {
       response.status(500).send('invalid token');
     }
     response.send(user);
@@ -35,13 +48,13 @@ app.get('/test', (request, response) => {
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
 
-//* ----- Source: jsonwebtoken docs. jwksUri endpoint from authO settings. ----- *//
+  //* ----- Source: jsonwebtoken docs. jwksUri endpoint from authO settings. ----- *//
 
   let jwksClient = require('jwks-rsa');
   let client = jwksClient({
     jwksUri: 'https://dev-wajr50en.us.auth0.com/.well-known/jwks.json'
   });
-  
+
   function getKey(header, callback) {
     client.getSigningKey(header.kid, function (error, key) {
       var signingKey = key.publicKey || key.rsaPublicKey;
@@ -51,6 +64,36 @@ app.get('/test', (request, response) => {
 
   // DONE STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 })
+
+// Connect to database
+mongoose.connect('mongodb://127.0.0.1:27017/books', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(async () => {
+    console.log('Connected to database');
+    // creates individual book data
+    let newBook = new BookModel({
+      title: 'Title of the book',
+      description: 'description of the book',
+      status: 'status',
+      email: 'email@email',
+    });
+    //saves created book object
+    await newBook.save();
+  });
+
+  // Clears the database - JP way
+  app.get('/clear', clear);
+  async function clear(request, response) {
+    try {
+      await BookModel.deleteMany({});
+      response.status(200).send('Database cleared');
+    }
+    catch (error) {
+      response.status(500).send('Error: Database not cleared');
+    }
+  };
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 

965f9b24b9bb57dfcb38f0927e9f00cd4e49a94d
Clarissa P <clarissapamonicutt@gmail.com>
2021-08-18 20:41:50 -0700

    lab11 finished

--- server.js
+++ server.js
@@ -5,20 +5,54 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
+const { request, response } = require('express');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
 
-  // TODO: 
+app.get('/test', (request, response) => {
+  response.send();
+
+  // DONE: 
   // STEP 1: get the jwt from the headers
+  // ? What are the headers? The const/requires?
+
+  const token = req.headers.authorizatoion.split(' ')[1];
+  
+  jwt.verify(token, getKey, {}, function (error, user){
+    if(error){
+      response.status(500).send('invalid token');
+    }
+    response.send(user);
+  });
+
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
+//* ----- Source: jsonwebtoken docs. jwksUri endpoint from authO settings. ----- *//
+
+  let jwksClient = require('jwks-rsa');
+  let client = jwksClient({
+    jwksUri: 'https://dev-wajr50en.us.auth0.com/.well-known/jwks.json'
+  });
+  
+  function getKey(header, callback) {
+    client.getSigningKey(header.kid, function (error, key) {
+      var signingKey = key.publicKey || key.rsaPublicKey;
+      callback(null, signingKey);
+    });
+  }
+
+  // DONE STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+//NOTE: jwks stands for JSON Web Key Set
+

59f62b6b1f447e3272d5f5d54d4a8c8785647649
Clarissa P <clarissapamonicutt@gmail.com>
2021-08-21 15:32:49 -0700

    finished lab12

---
+++ modules/seed.js
@@ -0,0 +1,36 @@
+'use strict';
+// Brings in books.js module
+const BookModel = require('../models/books');
+
+// creates and saves individual book data (Create R U D)
+async function addBook(object) {
+  let newBook = new BookModel(object);
+  return await newBook.save();
+};
+
+async function seed(request, response) {
+  let books = await BookModel.find({});
+  if (books.length === 0) {
+    await addBook({
+      title: 'Title of the book',
+      description: 'description of the book',
+      status: '200',
+      email: 'email@email',
+    });
+    await addBook({
+      title: 'Title of the book2',
+      description: 'description of the book2',
+      status: '2002',
+      email: 'email@email2',
+    });
+    await addBook({
+      title: 'Title of the book3',
+      description: 'description of the book3',
+      status: '2003',
+      email: 'email@email3',
+    });
+  };
+  response.send('seeded database');
+};
+
+module.exports = seed;
--- server.js
+++ server.js
@@ -3,99 +3,114 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+// token configurations for Auth0 
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
-const { request, response } = require('express');
+let client = jwksClient({
+  jwksUri: 'https://dev-wajr50en.us.auth0.com/.well-known/jwks.json'
+});
+
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function (error, key) {
+    let signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+};
+
+//connect to database with mongo
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/books', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// confirms or error handles mongoose connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected')
+});
+
+//*---------------------
 
 const app = express();
 app.use(cors());
 
+// allows us to access the request body
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
 
-const BookModel = require('./modules/books');
+// Brings in books.js module
+const BookModel = require('./models/books');
+const seed = require('./modules/seed');
 
-app.get('/books', async (request, response) => {
-  try {
-    let booksdb = await BookModel.find({});
-    response.status(200).send(booksdb);
-  }
-  catch (error) {
-    response.status(500).send('database error');
-  }
-});
+// *-------------------------------------------*
 
 app.get('/', (request, response) => {
   response.send('Hello World!')
-})
+});
 
 app.get('/test', (request, response) => {
-  response.send();
 
-  // DONE: 
   // STEP 1: get the jwt from the headers
-  // ? What are the headers? The const/requires?
-
-  const token = req.headers.authorizatoion.split(' ')[1];
-
+  const token = request.headers.authorization.split(' ')[1];
+  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+  // jsonwebtoken doc - https://www.npmjs.com/package/jsonwebtoken
   jwt.verify(token, getKey, {}, function (error, user) {
     if (error) {
-      response.status(500).send('invalid token');
+      response.status(500).send('Invalid Token');
     }
     response.send(user);
   });
+});
 
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
+// *-------------------------------------------*
 
-  //* ----- Source: jsonwebtoken docs. jwksUri endpoint from authO settings. ----- *//
-
-  let jwksClient = require('jwks-rsa');
-  let client = jwksClient({
-    jwksUri: 'https://dev-wajr50en.us.auth0.com/.well-known/jwks.json'
-  });
-
-  function getKey(header, callback) {
-    client.getSigningKey(header.kid, function (error, key) {
-      var signingKey = key.publicKey || key.rsaPublicKey;
-      callback(null, signingKey);
+app.get('/books', async (request, response) => {
+  try {
+    const token = request.headers.authorization.split(' ')[1];
+    // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+    // jsonwebtoken doc - https://www.npmjs.com/package/jsonwebtoken
+    jwt.verify(token, getKey, {}, function (error, user) {
+      if (error) {
+        response.status(500).send('Invalid Token');
+      }
+      // Read data (C READ U D)
+      // gets information from database
+      BookModel.find({}, (error, booksData) => {
+        response.status(200).send(booksData);
+      });
     });
   }
+  catch (error) {
+    response.status(500).send('Database Error');
+  }
+});
 
-  // DONE STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
-})
+//*-------------------------------------------*
 
-// Connect to database
-mongoose.connect('mongodb://127.0.0.1:27017/books', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(async () => {
-    console.log('Connected to database');
-    // creates individual book data
-    let newBook = new BookModel({
-      title: 'Title of the book',
-      description: 'description of the book',
-      status: 'status',
-      email: 'email@email',
-    });
-    //saves created book object
-    await newBook.save();
-  });
+// Seed database
+app.get('/seed', seed);
 
-  // Clears the database - JP way
-  app.get('/clear', clear);
-  async function clear(request, response) {
-    try {
-      await BookModel.deleteMany({});
-      response.status(200).send('Database cleared');
-    }
-    catch (error) {
-      response.status(500).send('Error: Database not cleared');
-    }
-  };
+// Clears the database - can go into it's own module
+app.get('/clear', clear);
+async function clear(request, response) {
+  try {
+    await BookModel.deleteMany({});
+    response.status(200).send('Database cleared');
+  }
+  catch (error) {
+    response.status(500).send('Error: Database not cleared');
+  }
+};
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 //NOTE: jwks stands for JSON Web Key Set
+
+//NOTE: beware jwt verify token error. Do console logs
+// Notes to ask Mark
+// How did youfigure out the seed status numbers
 
